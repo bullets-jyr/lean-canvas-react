fa5f40ec81de6d3ea8ba329c98f3203804412833
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-02-05 13:44:25 +0900

    레이아웃 UI

--- src/pages/CanvasDetail.jsx
+++ src/pages/CanvasDetail.jsx
@@ -1,18 +1,10 @@
-import { useLocation, useParams, useSearchParams } from 'react-router-dom';
-
+import CanvasTitle from '../components/CanvasTitle';
+import LeanCanvas from '../components/LeanCanvas';
 function CanvasDetail() {
-    const { id } = useParams();
-    const [searchParams] = useSearchParams();
-    console.log('searchParams: ', searchParams.get('keyword'));
-
-    const location = useLocation();
-    console.log('location: ', location);
     return (
         <div>
-            CanvasDetail
-            <p>id: {id}</p>
-            <p>keyword: {searchParams.get('keyword')}</p>
-            <p>hash: {location.hash}</p>
+            <CanvasTitle />
+            <LeanCanvas />
         </div>
     );
 }
