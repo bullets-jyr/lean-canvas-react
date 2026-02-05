668cbc16a1f099459fa3f2e7d545694992df4048
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-02-05 10:12:28 +0900

    React Router 란?

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './AppTailwindCSS.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-
+import App from './App';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: '',
+                element: <Home />,
+            },
+            {
+                path: 'about',
+                element: <About />,
+            },
+            {
+                path: 'contact',
+                element: <Contact />,
+            },
+        ],
+    },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>,
 );
